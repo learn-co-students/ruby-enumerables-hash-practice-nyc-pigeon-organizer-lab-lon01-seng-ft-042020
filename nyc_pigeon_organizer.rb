262aa9c1a9e5763d8c0140043f66acf9be00f83e
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-13 21:19:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+
+#def nyc_pigeon_organizer(data)
+#  new_hash = {}
+#  data.each do |atributes_name, value|
+#    value.each do |atributes_name, value|
+#
+#    end
+#  end
+#end
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name]||= {}
+        pigeon_list[name][attribute_name] ||= []
+        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+      end
+    end
+  end
+
+  pigeon_list
 end
